51027c404a3fec8b8a7fbc990bcec25c1d0826a5
Radu <rnedelcu93@gmail.com>
2024-10-01 06:27:42 -0700

    remove unused imports

--- src/main/java/com/example/employee_tracking/controllers/BookController.java
+++ src/main/java/com/example/employee_tracking/controllers/BookController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
